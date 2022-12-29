5292bb17cdbc7082c75c0e8b91518435f7190711
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2022-12-29 20:36:59 +0600

    Firebase Auth Initiated

--- src/firebase/firebase.cofig.js
+++ src/firebase/firebase.cofig.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCUhT418gidgPOQnT2ZZmXarwS54E1r8IY",
   authDomain: "allutilitytools.firebaseapp.com",
@@ -13,6 +10,6 @@ const firebaseConfig = {
   appId: "1:101603962570:web:38a7f247ad657a8957ce33"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export default app
